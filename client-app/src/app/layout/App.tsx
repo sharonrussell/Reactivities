190b3c4cef7474ef5ef2455fc8416a794da2bf77
sharonrussell <sharon.russell@comparethemarket.com>
2021-01-29 16:15:03 +0000

    Styling and react components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Header, Icon, List } from "semantic-ui-react";
+import React, { useState, useEffect, Fragment } from "react";
+import {Container, List} from "semantic-ui-react";
 import axios from "axios";
 import {IActivity} from "../models/activity";
+import NavBar from "../../features/nav/navbar";
 
 const App = () => {
 
@@ -16,17 +17,16 @@ const App = () => {
     }, []);
     
     return (
-      <div>
-        <Header as="h2">
-          <Icon name="users" />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
+      <Fragment>
+        <NavBar/>
+        <Container style={{marginTop: "7em"}}>
+            <List>
+                {activities.map((activity) => (
+                    <List.Item key={activity.id}>{activity.title}</List.Item>
+                ))}
+            </List>
+        </Container>
+      </Fragment>
     );
 }
 
